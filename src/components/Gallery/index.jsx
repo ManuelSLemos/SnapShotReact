b04f9bf939d088898f4079b4f0c1b07d6ab628b9
Manuel S. Lemos <m.lemos@geekshubs.com>
2021-01-13 13:23:21 +0100

    Api axios

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import Image from './Image';
 
 import './style.css';
 
-import api from '../../api/images.json';
 
-const Gallery = () => {
+class Gallery extends Component {
+    
+    constructor(props){
+        super(props);
 
-    return (
-        <section className="gallery-grid">
-            { api.photos.photo?.map( ({owner, id, title}) => (
-                <Image owned={owner} id={id} title={title} key={id} /> 
-            ))}
-        </section>
-    )
+        this.state = {
+            images: [],
+        }
+    }
+
+    componentDidMount = () => {
+
+        axios.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ae5995c1becdfbd7866a34ff6b593ed6&tags=mountain&per_page=24&format=json&nojsoncallback=1").then( api => {
+            this.setState({ images: api.data.photos.photo});
+        });
+
+    };
+
+    render(){
+        return (
+            <section className="gallery-grid">
+                { this.state.images?.map( ({owner, id, title}) => (
+                    <Image owned={owner} id={id} title={title} key={id} /> 
+                ))}
+            </section>
+        )
+    }
 }
 
 export default Gallery
